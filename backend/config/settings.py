63e5bd948c66ad9f4c5e0417ce6cfddf431b3f79
Muhammad Asad <asad.spartan300@gmail.com>
2026-08-29 01:42:12 +0500

    Strip whitespace from ALLOWED_HOSTS entries

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'change-this-in-production')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [h.strip() for h in os.environ.get('ALLOWED_HOSTS', '').split(',') if h.strip()]
 
 
 # Application definition
